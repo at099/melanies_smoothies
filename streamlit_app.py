dd91baf562e8abcb387abaab18e299e3715563ab
at099 <ankittomer07@gmail.com>
2024-06-20 15:48:20 +0530

    Update streamlit_app.py
    Adding json part

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ st.success("Your Smoothie is ordered! :name_on_order", icon="✅")
 #New section to display fruitvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
