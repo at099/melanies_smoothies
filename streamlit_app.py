62b44debdda5dddd54574b1749bd08c882a9e97a
at099 <ankittomer07@gmail.com>
2024-06-20 15:51:01 +0530

    Update streamlit_app.py
    create dataframe for fruitvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,5 @@ st.success("Your Smoothie is ordered! :name_on_order", icon="✅")
 #New section to display fruitvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=true)
